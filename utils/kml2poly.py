d6d80c0b67b67bbede86d613420dee6998db5f00
Pavel Podkorytov <pod.pavel@gmail.com>
2017-11-20 14:40:19 +0500

    utils/kml2poly.py reverse output points
    kml has clockwise points order, but CGAL want counterclockwise points
    order.

--- utils/kml2poly.py
+++ utils/kml2poly.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys
 import re
 import string
 
 res = []
 for line in sys.stdin:
-    m = re.match('(-)?\d+\.\d+,(-)?\d+\.\d+',line)
+    m = re.match('(-)?\d+\.\d+,(-)?\d+\.\d+', line)
     if m:
         res.append(m.group().translate(string.maketrans(',', ' ')))
 
 print len(res)
-for line in res:
-    print line
+
+# KML files from geofabrik has clockwise points order. CGAL need counterclockwise points order.
+for i in xrange(len(res)-1, 0, -1):
+    print res[i]
+
+# for line in res:
+#    print line
