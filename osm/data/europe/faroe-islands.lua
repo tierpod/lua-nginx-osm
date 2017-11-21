b6ca97ab3d58ecf6348e0eac8319a3e18e08ed4d
Pavel Podkorytov <pod.pavel@gmail.com>
2017-11-21 09:30:34 +0500

    data: update lua files, generate from counterclockwise source

--- osm/data/europe/faroe-islands.lua
+++ osm/data/europe/faroe-islands.lua
@@ -1,10 +1,10 @@
 local region = {
 {
  {lon=-8,lat=63},
- {lon=-6,lat=63},
- {lon=-6,lat=61.3},
  {lon=-8,lat=61.3},
- {lon=-8,lat=63},
+ {lon=-6,lat=61.3},
+ {lon=-6,lat=63},
+ {lon=-6,lat=63},
  {lon=-8,lat=63},
  },
 }
