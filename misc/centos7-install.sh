bf2fb944828adc03d23b5f0f90feeb5d5eec04cf
Pavel Podkorytov <pod.pavel@gmail.com>
2020-01-21 13:46:07 +0500

    Update misc/centos7-install.sh
    * Update postgres yum repo
    * Replace nginx-module-lua corp repo with openresty

--- misc/centos7-install.sh
+++ misc/centos7-install.sh
@@ -4,45 +4,18 @@
 set -eu
 
 yum install -y epel-release
-yum install -y tmux mc git vim lua lua-bitop lua-filesystem htop curl
+yum install -y tmux mc git lua lua-filesystem curl yum-utils make
 
 # CGAL-devel for building poly2lua.cpp, contains in official postgresql repo.
-REPO_FILE=/etc/yum.repos.d/pgdg-96-centos.repo
+REPO_FILE=/etc/yum.repos.d/pgdg-redhat-all.repo
 if ! [ -f "$REPO_FILE" ]; then
-  yum install -y https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
+  yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
   yum install -y CGAL-devel gcc-c++
 fi
 
-# https://nginx.ru/en/linux_packages.html
-# nginx mainline for nginx-module-lua, contains in official nginx repo.
-REPO_FILE=/etc/yum.repos.d/nginx.repo
+# nginx openresty https://openresty.org/en/linux-packages.html
+REPO_FILE=/etc/yum.repos.d/openresty.repo
 if ! [ -f "$REPO_FILE" ]; then
-  cat <<'EOF' > "$REPO_FILE"
-[nginx]
-name=nginx repo
-baseurl=http://nginx.org/packages/mainline/centos/7/$basearch/
-gpgcheck=0
-enabled=1
-EOF
-
-  yum install -y nginx
-fi
-
-# https://copr.fedorainfracloud.org/coprs/khara/nginx-module-ndk-lua/
-# nginx-module-lua for lua-nginx-osm, contains in unofficial copr repo.
-REPO_FILE=/etc/yum.repos.d/khara-nginx-module-ndk-lua-epel-7.repo
-if ! [ -f "$REPO_FILE" ]; then
-  cat <<'EOF' > "$REPO_FILE"
-[khara-nginx-module-ndk-lua]
-name=Copr repo for nginx-module-ndk-lua owned by khara
-baseurl=https://copr-be.cloud.fedoraproject.org/results/khara/nginx-module-ndk-lua/epel-7-$basearch/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://copr-be.cloud.fedoraproject.org/results/khara/nginx-module-ndk-lua/pubkey.gpg
-repo_gpgcheck=0
-enabled=1
-EOF
-
-  yum install -y nginx-module-lua
+  yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
+  yum install -y openresty
 fi
