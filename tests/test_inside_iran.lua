dbbe3137afa8d383d4015a2f4725ccd6241ad1d8
Pavel Podkorytov <pod.pavel@gmail.com>
2020-02-17 11:59:09 +0500

    tests: update iran, japan, philipinnes to use funcs module

--- tests/test_inside_iran.lua
+++ tests/test_inside_iran.lua
@@ -2,19 +2,7 @@
 
 package.path = '../?.lua'
 
-function table_print(t1)
-    indent_string = string.rep(" ", 4)
-    if t1 then
-        for _, b in pairs(t1) do
-            print('polygon:')
-            for _, v in pairs(b) do
-                print(indent_string,'lon=',v.lon,',lat=', v.lat)
-            end
-        end
-    end
-end
-
-local osm_tile = require "osm.tile"
+local funcs = require "tests.funcs"
 local osm_data = require "osm.data"
 
 local regname = "iran"
@@ -23,54 +11,13 @@ print('Region: ', regname)
 print('tile.data test:')
 local region = assert(osm_data.get_region(regname))
 print('  ok')
-table_print(region,0)
+funcs.table_print(region,0)
 
--- iran
-print('iran (inside?):')
-local z = 14
-local x = 10530
-local y = 6451
-assert(osm_tile.is_inside_region(region, x, y, z), 'expected true')
-print('  ok')
+funcs.test_inside(region, 'ir', 14, 10530, 6451)
+funcs.test_inside(region, 'ir', 10, 658, 403)
+funcs.test_inside(region, 'ir', 7, 85, 52)
 
-local z = 10
-local x = 658
-local y = 403
-assert(osm_tile.is_inside_region(region, x, y, z), 'expected true')
-print('  ok')
-
-local z = 7
-local x = 85
-local y = 52
-assert(osm_tile.is_inside_region(region, x, y, z), 'expected true')
-print('  ok')
-
-print('iran (outside?):')
-local z = 7
-local x = 79
-local y = 53
-assert(not osm_tile.is_inside_region(region, x, y, z), 'expected false')
-print('  ok')
-
-print('iran (outside?):')
-local z = 11
-local x = 1369
-local y = 797
-assert(not osm_tile.is_inside_region(region, x, y, z), 'expected false')
-print('  ok')
-
--- japan
-print('japan (outside?):')
-local z = 18
-local x = 233816
-local y = 100256
-assert(not osm_tile.is_inside_region(region, x, y, z), 'expected false')
-print('  ok')
-
--- kurgan
-print('kurgan (outside):')
-local z = 17
-local x = 89319
-local y = 41167
-assert(not osm_tile.is_inside_region(region, x, y, z), 'expected false')
-print('  ok')
+funcs.test_outside(region, '?', 7, 79, 53)
+funcs.test_outside(region, '?', 11, 1369, 797)
+funcs.test_outside(region, 'japan', 18, 233816, 100256)
+funcs.test_outside(region, 'ru kurgan', 17, 89319, 41167)
--- tests/test_inside_japan.lua
+++ tests/test_inside_japan.lua
@@ -2,19 +2,7 @@
 
 package.path = '../?.lua'
 
-function table_print(t1)
-    indent_string = string.rep(" ", 4)
-    if t1 then
-        for _, b in pairs(t1) do
-            print('polygon:')
-            for _, v in pairs(b) do
-                print(indent_string,'lon=',v.lon,',lat=', v.lat)
-            end
-        end
-    end
-end
-
-local osm_tile = require "osm.tile"
+local funcs = require "tests.funcs"
 local osm_data = require "osm.data"
 
 local regname = "japan"
@@ -23,28 +11,9 @@ print('Region: ', regname)
 print('tile.data test:')
 local region = assert(osm_data.get_region(regname), 'expected region coordinates')
 print('  ok')
-table_print(region,0)
+funcs.table_print(region,0)
 
-print('japan (inside?):')
-local z = 18
-local x = 233816
-local y = 100256
-assert(osm_tile.is_inside_region(region, x, y, z), 'expected true')
-print('  ok')
+funcs.test_inside(region, 'japan', 18, 233816, 100256)
 
-print('kurgan (outside?):')
-local z = 17
-local x = 89319
-local y = 41167
-
-assert(not osm_tile.is_inside_region(region, x, y, z), 'expected false')
-print('  ok')
-
-print('iran (outside?):')
-local z = 12
-local x = 2632
-local y = 1612
-
-assert(not osm_tile.is_inside_region(region, x, y, z), 'expected false')
-print('  ok')
-
+funcs.test_outside(region, 'ru kurgan', 17, 89319, 41167)
+funcs.test_outside(region, 'ir', 12, 2632, 1612)
